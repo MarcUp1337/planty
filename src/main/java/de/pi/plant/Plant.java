b22df62857f299be1f05ed0d72812f5d5fd85b59
Round Rabbit <roundrabbit@gmail.com>
2017-07-18 22:53:40 +0200

    fixed tests

--- src/main/java/de/pi/plant/Plant.java
+++ src/main/java/de/pi/plant/Plant.java
@@ -1,10 +1,62 @@
 package de.pi.plant;
 
-public interface Plant {
+public class Plant {
 
-	String getName();
+  private int waterPumpAddr;
+  private int spiChannel;
+  private String name;
 
-	int getWaterPumpAddr();
+  public Plant() {
+  }
 
-	int getSpiChannel();
+  public Plant(String name, int waterPumpAddr, int spiChannel) {
+
+    this.waterPumpAddr = waterPumpAddr;
+    this.spiChannel = spiChannel;
+    this.name = name;
+  }
+
+  public int getWaterPumpAddr() {
+    return waterPumpAddr;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public int getSpiChannel() {
+    return spiChannel;
+  }
+
+  public void setWaterPumpAddr(int waterPumpAddr) {
+    this.waterPumpAddr = waterPumpAddr;
+  }
+
+  public void setSpiChannel(int spiChannel) {
+    this.spiChannel = spiChannel;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+
+    Plant that = (Plant) o;
+
+    if (waterPumpAddr != that.waterPumpAddr) return false;
+    if (spiChannel != that.spiChannel) return false;
+    return name != null ? name.equals(that.name) : that.name == null;
+  }
+
+  @Override
+  public int hashCode() {
+    int result = waterPumpAddr;
+    result = 31 * result + spiChannel;
+    result = 31 * result + (name != null ? name.hashCode() : 0);
+    return result;
+  }
 }
--- src/test/java/de/pi/plant/impl/PlantAPITest.java
+++ src/test/java/de/pi/plant/impl/PlantAPITest.java
@@ -1,48 +1,68 @@
 package de.pi.plant.impl;
 
+import de.pi.plant.Plant;
+import de.pi.plant.PlantAPI;
+import de.piapi.gpio.SPIApi;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 
 /**
  * Created by Marcaroni on 11.07.2017.
  */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {SPIApi.class})
 public class PlantAPITest {
 
-    @Autowired
-    PlantAPI plantAPI;
+  PlantAPI plantAPI;
 
-    @Before
-    public void setUp() {
-        plantAPI = new PlantAPI();
-    }
+  @Mock
+  SPIApi spiApi;
 
-    @After
-    public void tearDown() {
-    }
+  @Before
+  public void setUp() {
+    MockitoAnnotations.initMocks(this);
+    plantAPI = new PlantAPI(spiApi);
+    Plant plantA = new Plant("PlantA", 0, 0);
+    plantAPI.addPlant(plantA);
+  }
 
-    @Test
-    public void getPlant() {
-        plantAPI.getPlant(0);
-    }
+  @After
+  public void tearDown() {
+  }
 
-    @Test
-    public void getHumidity() {
-        plantAPI.getHumidity(0);
-    }
+  @Test
+  public void getPlant() {
+    plantAPI.getPlant(0);
+  }
 
-    @Test
-    public void addPlant() {
-    }
+  @Test
+  public void getHumidity() {
+    Mockito.when(spiApi.getHumidity(Mockito.anyInt())).thenReturn( 40);
+    int actual = plantAPI.getHumidity(0).getBody();
+    Assert.assertEquals(40, actual);
+  }
 
-    @Test
-    public void waterPlant() {
-    }
+  @Test
+  public void addPlant() {
+    Plant plantB = new Plant("PlantB", 1, 1);
+    plantAPI.addPlant(plantB);
+    Assert.assertTrue(plantAPI.getPlants().contains(plantB));
+  }
 
-    @Test
-    public void getPlants() {
-    }
+  @Test
+  public void waterPlant() {
+  }
+
+  @Test
+  public void getPlants() {
+  }
 
 }
