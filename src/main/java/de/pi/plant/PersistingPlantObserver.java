0dd3eea948f55273bedee818ef9a4f55f68adad2
Round Rabbit <roundrabbit@gmail.com>
2017-07-30 10:38:07 +0200

    start stop script final

---
+++ src/main/java/de/pi/plant/PersistingPlantObserver.java
@@ -0,0 +1,7 @@
+package de.pi.plant;
+
+/**
+ * Created by Marcaroni on 21.07.2017.
+ */
+public class PersistingPlantObserver {
+}
---
+++ src/main/java/de/pi/plant/PlantObserver.java
@@ -0,0 +1,7 @@
+package de.pi.plant;
+
+/**
+ * Created by Marcaroni on 23.07.2017.
+ */
+public interface PlantObserver {
+}
