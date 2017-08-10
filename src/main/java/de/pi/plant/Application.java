d6e62e4b22a45376cd80e93d8e08e7df1f16a69e
MarcUp1337 <marcup1337@gmail.com>
2017-08-10 12:20:17 +0200

    cors and security configuration

--- src/main/java/de/pi/plant/Application.java
+++ src/main/java/de/pi/plant/Application.java
@@ -1,12 +1,10 @@
 package de.pi.plant;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@EnableAutoConfiguration
 @ComponentScan({"de.pi.plant", "de.plant"})
 public class Application {
 
